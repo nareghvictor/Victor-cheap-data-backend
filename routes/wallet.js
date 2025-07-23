e1cb817996b1728e7f37b16e4a71a73166094d68
naregh victor <nareghvictor@gmail.com>
2025-07-23 22:22:41 +0100

    Fix middleware path issue

--- routes/wallet.js
+++ routes/wallet.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const router = express.Router();
-const authMiddleware = require("../middlewares/auth");
+const authMiddleware = require("../middleware/auth");
 const User = require("../models/User");
 
 // Get wallet balance
